bab447576259d0d31bede89d788ee7c5ec56f1cb
Poulami Sengupta <PSengupta@oldmutual.com>
2024-11-26 12:43:25 +0200

    'Poulami4'

--- tests/KeywordHandler.spec.js
+++ tests/KeywordHandler.spec.js
@@ -14,6 +14,8 @@ if(scenarioSet.RunFlag === "Y")
 {
 test(`Scenario Executing - ${scenarioSet.ScenarioName}`,async ({browser})=>
 {
+    console.log("Hey Poulami");
+    console.log("Hey Antik");
     console.log("Hey Debasish!!");
     console.log("hey Ajay");
     
